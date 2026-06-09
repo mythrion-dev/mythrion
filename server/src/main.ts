38c7ac4d455174284090325970baefc41a7e9f7f
Danillo De Siena Okazaki <danillo.s.okazaki@gmail.com>
2026-06-09 19:01:55 -0300

    trying to fix cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -10,6 +10,8 @@ async function bootstrap() {
       process.env.FRONTEND_URL ?? 'https://mythrion-olive.vercel.app',
       'http://localhost:3001',
     ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
     credentials: true,
   })
 
