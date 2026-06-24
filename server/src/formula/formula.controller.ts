943ee26f01e472808985df000b7d178ac480b5ef
Lucas Verdi <lucas.a.verdi@gmail.com>
2026-06-24 17:20:05 -0300

    fix: add class-validator decorators to formula DTOs to prevent validation errors

--- server/src/formula/formula.controller.ts
+++ server/src/formula/formula.controller.ts
@@ -1,19 +1,29 @@
 import { Controller, Post, Body, UseGuards } from '@nestjs/common'
+import { IsString, IsObject, IsArray } from 'class-validator'
 import { FormulaService } from './formula.service.js'
 import { JwtAuthGuard } from '../auth/jwt-auth.guard.js'
 
 class EvaluateDto {
+  @IsString()
   formula!: string
+
+  @IsObject()
   variables!: Record<string, number>
 }
 
 class PreviewDto {
+  @IsString()
   formula!: string
+
+  @IsObject()
   variables!: Record<string, number>
 }
 
 class ValidateDto {
+  @IsString()
   formula!: string
+
+  @IsArray()
   variables!: string[]
 }
 
