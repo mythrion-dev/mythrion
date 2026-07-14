4f6e31a779c4d184f903e466056cdc98356ef85c
Lucas Verdi <lucas.a.verdi@gmail.com>
2026-07-14 19:58:40 -0300

    fix module images

--- server/src/image/image.module.ts
+++ server/src/image/image.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common'
+import { JwtModule } from '@nestjs/jwt'
 import { ImageController } from './image.controller.js'
 import { ImageService } from './image.service.js'
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET ?? 'dev-secret-change-me',
+      signOptions: { expiresIn: '7d' },
+    }),
+  ],
   controllers: [ImageController],
   providers: [ImageService],
   exports: [ImageService],
